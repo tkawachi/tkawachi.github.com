80f9c8ed430b35c37c692fd03198eac384a0b933
KAWACHI Takashi <kawachi@p-lucky.net>
2012-12-02 01:47:12 +0900

    Updated update.sh

--- update.sh
+++ update.sh
@@ -7,3 +7,4 @@ git pull octopress master     # Get the latest Octopress
 bundle install                # Keep gems updated
 rake update_source            # update the template's source
 rake update_style             # update the template's style
+git commit -a -m "Updated by octopress"
