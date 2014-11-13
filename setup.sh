27f0688fbb6772fe57cbeb50a6922fdf7d66c72d
Takashi Kawachi <tkawachi@gmail.com>
2014-11-14 00:36:43 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-bundle exec rake setup_github_pages
+set -e
+
+echo "git@github.com:tkawachi/tkawachi.github.com.git" | bundle exec rake setup_github_pages
+cd _deploy
+git fetch
+git reset --hard origin/master
